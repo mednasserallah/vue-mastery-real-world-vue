356ead11e98326524a81bf8762d6e4e20bfa8d63
Mohamed Nasser-allah <med.nasserallah@gmail.com>
2019-09-04 18:53:04 +0100

    Create a global icon component
    2. Real World Vue.js > 7. Global Components

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+// Base components
+import BaseIcon from '@/components/BaseIcon.vue';
+Vue.component('BaseIcon', BaseIcon);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
