fc5b06c0ce3488ccc3893ab01334f7904ad2f2b4
Mohamed Nasser-allah <med.nasserallah@gmail.com>
2019-09-05 01:12:16 +0100

    Set up Axios and make API calls
    2. Real World Vue.js > 9. API calls with Axios

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,15 +1,33 @@
 <template>
     <div>
         <h1>Event List</h1>
-        <EventCard />
+        <EventCard v-for="event in events" :key="event.id" :event="event" />
     </div>
 </template>
 
 <script>
+import EventService from '@/services/EventService.js';
 import EventCard from '@/components/EventCard';
+
 export default {
     components: {
         EventCard
+    },
+
+    data() {
+        return {
+            events: []
+        };
+    },
+
+    created() {
+        EventService.getEvents()
+            .then(response => {
+                this.events = response.data;
+            })
+            .catch(error => {
+                console.log('There was an error:' + error.response);
+            });
     }
 };
 </script>
